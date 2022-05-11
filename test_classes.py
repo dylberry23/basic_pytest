147cdbcd403f9789ebabfd646090775c0b2bcf90
Dylan Berry <dylberry92@gmail.com>
2022-05-10 22:03:37 -0400

    "pytest fixtures"

--- test_classes.py
+++ test_classes.py
@@ -23,7 +23,7 @@ def test_classes_compare():
 
 
 
-class Person:
+class Person1:
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
@@ -36,13 +36,13 @@ class Person:
         return False
 
 
-john = Person('John', 'Doe', 25)
-jane = Person('Jane', 'Doe', 25)
-mary = Person('Mary', 'Doe', 27)
+john = Person1('John', 'Doe', 25)
+jane = Person1('Jane', 'Doe', 25)
+mary = Person1('Mary', 'Doe', 27)
 
 print(john == jane)  # True
 print(john == mary)  # False
 
 
-john = Person('John', 'Doe', 25)
+john = Person1('John', 'Doe', 25)
 print(john == 20)  # False
